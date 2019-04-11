3eb682fde979e9870d2c9b2d5dcde9f70bc4b44e
Geeknerd1337 <geeknerd1337@gmail.com>
2019-04-11 02:24:51 -0400

    New Commit

---
+++ Shader Test/Assets/Scripting/Messages System/MessageMaster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageMaster : MonoBehaviour {
+
+
+
+    public Message[] messages;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
