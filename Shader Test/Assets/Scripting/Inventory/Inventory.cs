d469b1386e395abd730ec109b15a0a5c8b31eecd
Geeknerd1337 <geeknerd1337@gmail.com>
2019-04-25 00:54:32 -0400

    UI set up

---
+++ Shader Test/Assets/Scripting/Inventory/Inventory.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    public List<Item> items = new List<Item>();
+    public List<int> quants = new List<int>();
+
+    #region Singleton
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("More than one inventory in scene");
+            return;
+        }
+        instance = this;
+    }
+    #endregion
+
+    public delegate void OnItemChanged();
+    [SerializeField]
+    public OnItemChanged OnItemChangedCallback;
+
+
+    public int space = 20;
+
+    public bool Add(Item item)
+    {
+        if(items.Count >= space)
+        {
+            return false;
+        }
+
+        if (!items.Contains(item))
+        {
+            items.Add(item);
+            quants.Add(1);
+        }
+        else
+        {
+            for(var i = 0; i < items.Count; i++)
+            {
+                if(items[i] == item)
+                {
+                    quants[i]++;
+                }
+            }
+        }
+
+        if (OnItemChangedCallback != null)
+        {
+            OnItemChangedCallback.Invoke();
+        }
+        return true;
+    }
+
+    public void Remove(Item item)
+    {
+        for(int i = 0; i < items.Count; i++)
+        {
+            if(items[i] == item)
+            {
+                if(quants[i] > 1)
+                {
+                    quants[i]--;
+                }
+                else
+                {
+                    quants.RemoveAt(i);
+                    items.Remove(item);
+                }
+            }
+        }   
+
+        if (OnItemChangedCallback != null)
+        {
+            Debug.Log("Hello4");
+            OnItemChangedCallback.Invoke();
+        }
+        
+    }
+}
---
+++ Shader Test/Assets/Scripting/Inventory/Item.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject {
+
+    new public string name = "New Item";
+    public Sprite icon = null;
+    public bool isDefaultItem = false;
+    public bool consumable = false;
+
+    [TextArea(3,10)]
+    public string description = "Item Description";
+
+    public virtual void Use()
+    {
+        //Use the item
+        Debug.Log("Using" + name);
+    }
+
+}
