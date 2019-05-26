83ceb9169b87a7c28bf2944ed2ab3f71e58eb922
mentalpop <43460243+mentalpop@users.noreply.github.com>
2019-05-25 23:01:35 -0400

    All these ding daang changes

---
+++ Shader Test/Assets/My Assets/Scripting/Kaitlyn's not a programmer but she tries/Sense.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+
+public class Sense : MonoBehaviour
+{
+    public float checkRadius;
+    public LayerMask checkLayers;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Sense"))
+        {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, checkRadius, checkLayers);
+            Array.Sort(colliders, new DistanceComparer(transform));
+
+            foreach (Collider item in colliders)
+            {
+                Debug.Log(item.name);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, checkRadius);
+    }
+
+}
