f9063176c5cc0a692dc5ef4646bb3ae22a1d37f7
mentalpop <43460243+mentalpop@users.noreply.github.com>
2019-06-25 16:06:12 -0400

    Flower chimee

---
+++ Shader Test/Assets/FlowerChime.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlowerChime : MonoBehaviour
+{
+
+    public AudioClip flowerBumble;
+    public bool alreadyPlayed = false;
+
+
+    private AudioSource source;
+    private float lowPitchRange = .75F;
+    private float highPitchRange = 1.5F;
+
+  
+
+
+    void Start()
+    {
+
+        source = GetComponent<AudioSource>();
+    }
+
+
+    void OnTriggerEnter()
+    {
+        Debug.Log("it trigger");
+        if (!alreadyPlayed)
+        {
+            source.pitch = Random.Range(lowPitchRange, highPitchRange);
+            GetComponent<AudioSource>().Play();
+            alreadyPlayed = true;
+        }
+      
+    }
+
+}
