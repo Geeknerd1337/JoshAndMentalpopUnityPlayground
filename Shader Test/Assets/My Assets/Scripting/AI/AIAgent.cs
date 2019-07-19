3e2ff669ba568a13e9834ab39b109d2545fca259
Geeknerd1337 <geeknerd1337@gmail.com>
2019-07-18 20:12:30 -0400

    Changes to npcs

--- Shader Test/Assets/My Assets/Scripting/AI/AIAgent.cs
+++ Shader Test/Assets/My Assets/Scripting/AI/AIAgent.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
 public class AIAgent : MonoBehaviour
 {
 
@@ -11,26 +12,43 @@ public class AIAgent : MonoBehaviour
     public List<Transform> nodes;
 
     private int index;
+
+    public ThirdPersonCharacter character;
     // Start is called before the first frame update
     void Start()
     {
         index = 0;
         agent = GetComponent<NavMeshAgent>();
         agent.SetDestination(nodes[index].position);
+        agent.updateRotation = false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(agent.remainingDistance < agent.stoppingDistance)
+        MoveNPC();
+    }
+
+
+    void MoveNPC()
+    {
+        if (agent.remainingDistance < agent.stoppingDistance)
         {
+            character.Move(Vector3.zero, false, false);
+            Debug.Log("Reached this node");
             index++;
-            if(index >= nodes.Count)
+            if (index >= nodes.Count)
             {
                 index = 0;
             }
             agent.SetDestination(nodes[index].position);
         }
+
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+
     }
 }
